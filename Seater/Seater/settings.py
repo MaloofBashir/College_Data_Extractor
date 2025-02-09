775380e0cdbfd28e8b448dabfdcf31ca5d11d529
Maloof <maloof700@gmail.com>
2025-02-10 01:07:26 +0530

    adding

--- Seater/Seater/settings.py
+++ Seater/Seater/settings.py
@@ -123,12 +123,11 @@ USE_TZ = True
 
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/Seating_Plan/static')  # Required for collectstatic
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Use a writable directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Optional: Global static files
+]
 
 
 # Default primary key field type
