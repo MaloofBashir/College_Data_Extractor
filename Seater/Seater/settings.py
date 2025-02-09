803a2bc0d4ae15bbdf63cdc4087823a5601c079f
Maloof <maloof700@gmail.com>
2025-02-10 00:38:51 +0530

    adding

--- Seater/Seater/settings.py
+++ Seater/Seater/settings.py
@@ -121,8 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
